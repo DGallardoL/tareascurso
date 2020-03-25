a95aff37bfee3e93cc4dce3d745a800288c263e4
DGallardoL <62126483+DGallardoL@users.noreply.github.com>
2020-03-24 20:25:46 -0600

    Ejercicios
    .

---
+++ ejercicios_print.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 19:48:02 2020
+
+@author: aaaga
+"""
+
+
+pi = input("introduzca pi con seis decimales: ")
+pi = float(pi)
+#print(%.6 "este es el valor de pi: \n %.2f pi"%(pi,pi))
+#print(%.6 "este es el valor de pi: \n %.2f pi"%(pi,pi))
+print("valor original de pi : %.6f,\neste es el valor de pi con dos decimales %.2f"  % (pi,pi))
